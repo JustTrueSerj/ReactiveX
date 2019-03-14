07d2f170fe06f5af6469e8cdb20bbd09286adb47
Sergey Borodin <justtrueserj@gmail.com>
2019-03-14 19:01:14 +0300

    First quest done.

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
-import {Observable, of} from 'rxjs';
+import {Observable, of, timer} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-box',
@@ -8,10 +9,22 @@ import {Observable, of} from 'rxjs';
   styleUrls: ['./search-box.component.scss']
 })
 export class SearchBoxComponent implements OnInit {
-  field = new FormControl('', [
-    Validators.minLength(3)
-  ]);
+  field = new FormControl('');
 
   ngOnInit() {
+    of(this.field.valueChanges)
+      .subscribe({
+        next: subscriber => {
+          const arrOfLoggedWords = [];
+          subscriber.subscribe(value => {
+            if ((value.length > 3) && (arrOfLoggedWords.every(arg => arg !== value))) {
+              timer(500)
+                .pipe(take(1))
+                .subscribe(() => console.log(value));
+            }
+            arrOfLoggedWords.push(value);
+          });
+        }
+      });
   }
 }
