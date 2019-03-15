01a166b6a371487fe4fd0ac7f2e1d9295273e27e
Sergey Borodin <justtrueserj@gmail.com>
2019-03-15 11:49:50 +0300

    Add unsub

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,22 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
-import {Observable, of, timer} from 'rxjs';
-import {distinctUntilChanged, take, debounceTime, filter} from 'rxjs/operators';
+import {Observable, of, timer, ReplaySubject} from 'rxjs';
+import {distinctUntilChanged, take, debounceTime, filter, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   field = new FormControl('');
+  destroy: ReplaySubject<any> = new ReplaySubject<any>(1);
+
 
   ngOnInit() {
     this.field.valueChanges.pipe(
       debounceTime(500),
       distinctUntilChanged(),
-      filter((fieldValue) => fieldValue.length > 3))
-
+      filter((fieldValue) => fieldValue.length > 3),
+      takeUntil(this.destroy))
       .subscribe(value => console.log(value));
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(null);
+    this.destroy.complete();
   }
 }
