b25d4fcff9b47ca3f15217ac94f653852d466611
Sergey Borodin <justtrueserj@gmail.com>
2019-03-18 15:25:05 +0300

    Fixed some troubles

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -11,43 +11,30 @@ import {HttpService} from './Http.service';
 })
 export class SearchBoxComponent implements OnInit, OnDestroy {
   field = new FormControl('');
-  destroy: ReplaySubject<any> = new ReplaySubject<any>(1);
+  destroy$: ReplaySubject<any> = new ReplaySubject<any>(1);
   searchResults = [];
 
   constructor(private http: HttpService) {
   }
 
-
   ngOnInit() {
     this.field.valueChanges.pipe(
       debounceTime(500),
       distinctUntilChanged(),
-      filter(fieldValue => fieldValue > 3),
-      switchMap((fieldValue: string) => {
-        return this.http.loadVideosSuggestions(fieldValue);
+      filter(value => value > 3),
+      switchMap((value: string) => {
+        return this.http.loadVideosSuggestions(value);
       }),
-      takeUntil(this.destroy))
+      takeUntil(this.destroy$))
       .subscribe(httpResult => {
-          this.searchResults.length = 0;
-          this.searchResults = httpResult.items.map(res => res);
+          this.searchResults = httpResult.items;
           console.log(this.searchResults);
         }
       );
-
-    // this.field.valueChanges.pipe(
-    //   debounceTime(500),
-    //   distinctUntilChanged(),
-    //   filter((fieldValue) => fieldValue.length > 3),
-    //   takeUntil(this.destroy))
-    //   .subscribe((fieldValue$) => this.http.loadVideosSuggestions(fieldValue$).subscribe(httpResult => {
-    //     this.searchResults.length = 0;
-    //     this.searchResults = httpResult.items.map(res => res);
-    //     console.log(this.searchResults);
-    //   }));
   }
 
   ngOnDestroy() {
-    this.destroy.next(null);
-    this.destroy.complete();
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
 }
