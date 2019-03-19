bd9e35d8029e0b403f32b880b03d6885f48cf902
Sergey Borodin <justtrueserj@gmail.com>
2019-03-19 14:02:39 +0300

    Have a trouble now

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {Observable, of, timer, Subject, ReplaySubject} from 'rxjs';
 import {distinctUntilChanged, take, debounceTime, filter, takeUntil, switchMap} from 'rxjs/operators';
@@ -9,30 +9,18 @@ import {HttpService} from './Http.service';
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent implements OnInit, OnDestroy {
+export class SearchBoxComponent implements OnInit {
   field = new FormControl('');
-  destroy$: Subject<any> = new Subject<any>();
-  searchResults = [];
+  searchResults: Observable<any>;
 
   constructor(private http: HttpService) {
   }
 
   ngOnInit() {
-    this.field.valueChanges.pipe(
+    this.searchResults = this.field.valueChanges.pipe(
       debounceTime(500),
       distinctUntilChanged(),
       filter(value => value > 3),
-      switchMap((value) => this.http.loadVideosSuggestions(value)),
-      takeUntil(this.destroy$))
-      .subscribe(httpResult => {
-          this.searchResults = httpResult.items;
-          console.log(this.searchResults);
-        }
-      );
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(null);
-    this.destroy$.complete();
+      switchMap((value) => this.http.loadVideosSuggestions(value)));
   }
 }
