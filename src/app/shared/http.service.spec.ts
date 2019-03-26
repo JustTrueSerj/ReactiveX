240c5c2814669df4f564e1f72aa64710d2de3646
Sergey Borodin <justtrueserj@gmail.com>
2019-03-26 14:48:49 +0300

    Servie test returns true

--- src/app/shared/http.service.spec.ts
+++ src/app/shared/http.service.spec.ts
@@ -1,43 +1,27 @@
-import {HttpService} from './http.service';
 import {TestBed} from '@angular/core/testing';
-import {Observable, of} from 'rxjs';
-import {ResponseResultModel} from './response-result.model';
 import {ItemsModel} from './items.model';
+import {Observable, of} from 'rxjs';
 
 class MockHttpService {
-  static loadVideosSuggestions(searchString: string): Observable<{ etag: string; items: { etag: string; id: { kind: string; videoId: string }[]; kind: string }[]; kind: string; nextPageToken: string; pageInfo: {}; regionCode: string }> {
-    return of(mockResponseResult);
+  static loadVideosSuggestions(searchString: string): Observable<ItemsModel[]> {
+    console.log(searchString);
+    return of(expectedData);
   }
 }
 
-let testService: MockHttpService;
-let mockResponseResult: { etag: string; items: { etag: string; id: { kind: string; videoId: string }[]; kind: string }[]; kind: string; nextPageToken: string; pageInfo: {}; regionCode: string };
+const expectedData: ItemsModel[] = [
+  {etag: '123', kind: '1234', id: null},
+  {etag: '1234', kind: '12345', id: null},
+  {etag: '12345', kind: '123456', id: null}
+];
 
 describe('Http Service', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-       providers: [MockHttpService]
-    });
-    testService = TestBed.get(MockHttpService);
-    mockResponseResult = {
-      etag: '123',
-      items: [
-        {
-          etag: '123',
-          id: [{
-            kind: '123',
-            videoId: '11111'
-          }],
-          kind: '123'
-        }],
-      kind: '1234',
-      nextPageToken: '12345',
-      pageInfo: {},
-      regionCode: '12345677'
-    };
+    TestBed.configureTestingModule({});
   });
-
-  it('Should return Observable', () => {
-    expect(MockHttpService.loadVideosSuggestions('123')).toEqual(of(mockResponseResult));
+  it('should returns Observable of ItemsModel', () => {
+    MockHttpService.loadVideosSuggestions('123').subscribe((value) => {
+      expect(value).toEqual(expectedData);
+    });
   });
 });
