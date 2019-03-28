89cbf87d548afb53e721c789d77424b7c42fca1e
Sergey Borodin <justtrueserj@gmail.com>
2019-03-28 10:45:02 +0300

    Fixed *label

--- src/app/shared/label.directive.ts
+++ src/app/shared/label.directive.ts
@@ -4,19 +4,18 @@ import {AfterContentInit, Directive, DoCheck, ElementRef, Input, OnChanges, OnIn
   selector: '[label]'
 })
 export class LabelDirective implements AfterContentInit {
-  value: any;
+  value: string;
 
   constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef<any>) {
   }
 
   @Input() set label(value) {
     this.value = value;
+    this.viewContainer.clear();
     this.viewContainer.createEmbeddedView(this.templateRef);
   }
 
   ngAfterContentInit() {
-    this.viewContainer.get(0).rootNodes[0].value = this.value;
-    console.log(this.viewContainer.get(0).rootNodes[0].value);
-    console.log(this.templateRef);
+    this.viewContainer.get(0).rootNodes[0].placeholder = this.value;
   }
 }
