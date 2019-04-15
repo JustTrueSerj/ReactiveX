942cebd1591c36a5b51dd30d74e53e65e1c87f25
Sergey Borodin <justtrueserj@gmail.com>
2019-04-15 18:51:30 +0300

    Mission complete(2)

--- src/app/ngrx/reducers/result.reducer.ts
+++ src/app/ngrx/reducers/result.reducer.ts
@@ -16,7 +16,11 @@ export function resultReducer(state = initialState, action: VideoActions) {
       lastRequest = action.payload;
       return {...state, videos: action.payload};
     case FILTER_RESULT:
-      return {...state, videos: lastRequest.filter(x => x.etag === action.payload)};
+      return {
+        ...state, videos: lastRequest.filter(x => action.payload !== 'All'
+          ? x.etag === action.payload
+          : x)
+      };
     default:
       return state;
   }
--- src/app/wrapper/radio-changer/radio-changer.module.ts
+++ src/app/wrapper/radio-changer/radio-changer.module.ts
@@ -1,15 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RadioChangerComponent} from './radio-changer.component';
 import {CommonModule} from '@angular/common';
-import {EffectsModule} from '@ngrx/effects';
-import {VideoEffects} from '../../ngrx/effects/video.effects';
 import {StoreModule} from '@ngrx/store';
 import {resultReducer} from '../../ngrx/reducers/result.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
-    EffectsModule.forRoot([VideoEffects]),
     StoreModule.forRoot({videos: resultReducer})
   ],
   declarations: [
